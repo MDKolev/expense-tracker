a7ea7a0f8e93590640483d1699a6ee76faec9a66
MDKolev <113345588+MDKolev@users.noreply.github.com>
2024-06-29 15:52:07 +0300

    setting up firebase

---
+++ src/firebase-config/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCQxOOLWoXgiEFiYbvKanl2uGfLkL-Gxvc",
+  authDomain: "expense-tracker-a0da0.firebaseapp.com",
+  projectId: "expense-tracker-a0da0",
+  storageBucket: "expense-tracker-a0da0.appspot.com",
+  messagingSenderId: "724309687308",
+  appId: "1:724309687308:web:87a56b1219d56b7d9b0229"
+};
+
+const app = initializeApp(firebaseConfig);
