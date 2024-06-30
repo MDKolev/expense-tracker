66f615795f327460c1e0843c76ce11203db6ecf1
MDKolev <113345588+MDKolev@users.noreply.github.com>
2024-06-30 21:56:54 +0300

    implementing Settings component

---
+++ src/components/features/Settings.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import "./settings.css";
+import profilePic from "../../assets/profile-pic.jpeg";
+
+const Settings = () => {
+  return (
+    <>
+      <div className="feature-header">
+        <h1>Settings</h1>
+      </div>
+      <div className="settings-container">
+        <div className="account-info">
+          <div className="personal-info-container">
+            <h2>Personal Information</h2>
+            <h3>Username</h3>
+            <p>username</p>
+            <h3>Email</h3>
+            <p>email</p>
+            <h3>Joined</h3>
+            <p>30/06/2024</p>
+          </div>
+          <div className="profile-pic-container">
+            <h2>Change Avatar</h2>
+            <img src={profilePic} alt="" />
+            <button className="save-btn">Save Avatar</button>
+          </div>
+        </div>
+
+        <div className="appearance-container">
+          <h2>Appearance</h2>
+          <div className="appearance-modes">
+            <div className="light">
+              <p>Light</p>
+              <label class="switch">
+                <input type="checkbox" />
+                <span class="slider round"></span>
+              </label>
+            </div>
+            <div className="dark">
+              <p>Dark</p>
+              <label class="switch">
+                <input type="checkbox" />
+                <span class="slider round"></span>
+              </label>
+            </div>
+            <div className="dawn">
+              <p>Dawn</p>
+              <label class="switch">
+                <input type="checkbox" />
+                <span class="slider round"></span>
+              </label>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Settings;
