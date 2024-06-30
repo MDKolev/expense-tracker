8645238640373ea89fefdbb2ae568d6031bb7999
MDKolev <113345588+MDKolev@users.noreply.github.com>
2024-06-30 22:10:15 +0300

    implemented switch between features

--- src/components/main/Home.jsx
+++ src/components/main/Home.jsx
@@ -1,26 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./home.css";
 import profilePic from "../../assets/profile-pic.jpeg";
 import { GiMoneyStack } from "react-icons/gi";
 import { FiSettings } from "react-icons/fi";
 import Expenses from "../features/Expenses";
+import Settings from "../features/Settings";
 
 const Home = () => {
+  const [isActive, setIsActive] = useState(true);
+  const [isExpensesActive, setIsExpensesActive] = useState(false)
+  const [isSettingsActive, setIsSettingsActive] = useState(true)
+
+  const handleToggle = () => {
+    setIsExpensesActive((prevExpensesActive) => !prevExpensesActive);
+    setIsSettingsActive((prevSettingsActive) => !prevSettingsActive);
+  };
+
   return (
     <div className="home-container">
       <div className="sidebar">
         <img src={profilePic} alt="" className="profile-pic" />
         <span>Example Username</span>
-        <button className="feature-btn">
+        <button className="feature-btn" onClick={handleToggle}>
           <GiMoneyStack className="feature-icon" /> Expenses
         </button>
-        <button className="feature-btn">
+        <button className="feature-btn" onClick={handleToggle}>
           <FiSettings className="feature-icon" /> Settings
         </button>
       </div>
 
       <div className="feature-window">
-        <Expenses/>
+        {isExpensesActive && <Expenses />}
+        {isSettingsActive && <Settings />}
       </div>
     </div>
   );
