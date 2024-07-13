02abbabbfafcd891b380fefe07331592f1ed9d71
MDKolev <113345588+MDKolev@users.noreply.github.com>
2024-07-13 10:02:15 +0300

    implemented logout

--- src/components/main/Home.jsx
+++ src/components/main/Home.jsx
@@ -5,27 +5,53 @@ import { GiMoneyStack } from "react-icons/gi";
 import { FiSettings } from "react-icons/fi";
 import Expenses from "../features/Expenses";
 import Settings from "../features/Settings";
+import { IoLogOutOutline } from "react-icons/io5";
+import { signOut } from "firebase/auth";
+import { auth } from "../../firebase-config/firebase";
+import { useNavigate } from "react-router-dom";
 
-const Home = () => {
+
+const Home = ({ emailOfUser }) => {
   const [isActive, setIsActive] = useState(true);
-  const [isExpensesActive, setIsExpensesActive] = useState(false)
-  const [isSettingsActive, setIsSettingsActive] = useState(true)
+  const [isExpensesActive, setIsExpensesActive] = useState(true);
+  const [isSettingsActive, setIsSettingsActive] = useState(false);
+  const navigate = useNavigate();
 
   const handleToggle = () => {
     setIsExpensesActive((prevExpensesActive) => !prevExpensesActive);
     setIsSettingsActive((prevSettingsActive) => !prevSettingsActive);
   };
 
+  const handleClick = () => {
+    console.log(emailOfUser);
+  };
+
+  const handleLogout = async() => {
+    try {
+      await signOut(auth);
+      navigate("/")
+    } catch(err) {
+      console.error(err);
+    }
+  }
+
   return (
     <div className="home-container">
       <div className="sidebar">
-        <img src={profilePic} alt="" className="profile-pic" />
-        <span>Example Username</span>
-        <button className="feature-btn" onClick={handleToggle}>
-          <GiMoneyStack className="feature-icon" /> Expenses
-        </button>
-        <button className="feature-btn" onClick={handleToggle}>
-          <FiSettings className="feature-icon" /> Settings
+        <div>
+          <img src={profilePic} alt="" className="profile-pic" />
+          <span>Example Username</span>
+
+          <button className="feature-btn" onClick={handleToggle}>
+            <GiMoneyStack className="feature-icon" /> Expenses
+          </button>
+          <button className="feature-btn" onClick={handleToggle}>
+            <FiSettings className="feature-icon" /> Settings
+          </button>
+        </div>
+
+        <button className="logout-btn" onClick={handleLogout} >
+          <IoLogOutOutline className="logout-icon" /> Logout
         </button>
       </div>
 
